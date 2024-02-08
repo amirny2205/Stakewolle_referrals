ccd2931848a3ffaaa419c5beb2d03a48eed5efc4
user <user@localhost.localdomain>
2024-02-08 14:36:17 +0300

    referral code w\o the requirement that the only one can be active

--- users/models.py
+++ users/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+import random
+
+
+class ReferralCode(models.Model):
+    code_str = models.CharField(max_length=64,
+                                primary_key=True,
+                                default=lambda: "".join(
+                                    [chr(random.randrange(48, 123))
+                                     for y in range(64)]))
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
